8c4afdfc18604a1cb92a781d3b7d293e9c206838
camiloarceg <camiloarceg@gmail.com>
2024-08-03 17:11:11 -0500

    add 5 task

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise.
+
+    Args:
+        mat1 (list): The first 2D matrix.
+        mat2 (list): The second 2D matrix.
+
+    Returns:
+        list: The result of adding the two matrices,
+        or None if the matrices have different shapes.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+
+    return result
