b2f2223d9ab18ea60547f55079847c8b794a72b6
camiloarceg <camiloarceg@gmail.com>
2024-08-03 17:58:21 -0500

    add 8 task

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+""" ridin bareback
+"""
+
+
+def mat_mul(mat1, mat2):
+    """ multiplies 2 2D matrix
+    """
+    rows_mat1 = len(mat1)
+    cols_mat1 = len(mat1[0])
+    rows_mat2 = len(mat2)
+    cols_mat2 = len(mat2[0])
+
+    if cols_mat1 != rows_mat2:
+        return None
+
+    result = [[0 for _ in range(cols_mat2)] for _ in range(rows_mat1)]
+
+    for i in range(rows_mat1):
+        for j in range(cols_mat2):
+            for k in range(cols_mat1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
