e6a18067b1ed4792f761d1641aa308708e3ecf3a
camiloarceg <camiloarceg@gmail.com>
2024-08-03 15:55:33 -0500

    add 2 task

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    """ computes a matrix shape
+
+    Args:
+        matrix (matrix): the matrix to compute the shape of
+    """
+
+    if not isinstance(matrix, list):
+        return []
+    return [len(matrix)] + matrix_shape(matrix[0])
