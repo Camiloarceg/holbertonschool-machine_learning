1e1dd1a26a57bbcd3ceaf24c0368f59351a62163
camiloarceg <camiloarceg@gmail.com>
2024-08-04 19:24:46 -0500

    add task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" the western exchanges
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a given numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input matrix.
+
+    Returns:
+        numpy.ndarray: The transposed matrix.
+    """
+    return matrix.transpose()
