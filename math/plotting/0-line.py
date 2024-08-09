dbc540b0a357175039779ee31feb58f053b8620b
camiloarceg <camiloarceg@gmail.com>
2024-08-08 21:41:48 -0500

    add task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+""" line
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """ plots y as a line
+    """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
