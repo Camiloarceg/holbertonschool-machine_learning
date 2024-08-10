e4811b379ede4dcab49c3ba3ef90b5da72e5a2cb
camiloarceg <camiloarceg@gmail.com>
2024-08-10 13:27:06 -0500

    fix pycodestyle

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,19 +4,20 @@
 
 
 def poly_integral(poly, C=0):
-    """ calculates the integral of a polynomial
+    """ calculates ply integrals
     """
-    if not isinstance(poly, list) or not all(
-           isinstance(coeff, int) for coeff in poly):
+    if not isinstance(poly, list) or not all(isinstance(coeff, int)
+                                             for coeff in poly):
         return None
     if not isinstance(C, int):
         return None
 
     integral_poly = [C]
+
     for power, coeff in enumerate(poly):
         new_coeff = coeff / (power + 1)
-        integral_poly.append(
-            int(new_coeff) if new_coeff.is_integer() else new_coeff)
+        integral_poly.append(int(new_coeff) if new_coeff.is_integer()
+                             else new_coeff)
 
     while len(integral_poly) > 1 and integral_poly[-1] == 0:
         integral_poly.pop()
