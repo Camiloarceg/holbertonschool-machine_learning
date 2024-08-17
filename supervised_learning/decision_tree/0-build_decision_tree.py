a201eeefd5b76d3579469815b1a554c9978de48f
camiloarceg <camiloarceg@gmail.com>
2024-08-17 10:52:33 -0500

    fix documentation

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" build decisoon tree
+"""
 
 import numpy as np
 
@@ -28,8 +30,6 @@ class Node:
 
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
-        """ The constructor
-        """
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -70,8 +70,6 @@ class Leaf(Node):
     """
 
     def __init__(self, value, depth=None):
-        """ The constructor
-        """
         super().__init__()
         self.value = value
         self.is_leaf = True
@@ -107,8 +105,6 @@ class Decision_Tree:
 
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
-        """ The constructor
-        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
